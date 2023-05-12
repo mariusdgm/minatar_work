be17e76539721599e950aad0f3814ebe4b077b89
mariusdgm <dragomir10m@yahoo.com>
2023-05-12 14:52:38 +0300

    fixed some renduntant code and added point of interest

--- minatar_dqn/redo.py
+++ minatar_dqn/redo.py
@@ -14,7 +14,7 @@ class ReDo:
         self.tau = tau
         self.beta = beta
         self.running_avg = None
-
+        
     def __call__(self, m, _, output):
         """Keep a running average of abs(activation)"""
         dims = [0] if output.ndim == 2 else [0, -2, -1]
@@ -32,12 +32,18 @@ class ReDo:
         """
         score = self.get_score()
         idxs = (score <= self.tau).nonzero().flatten()
+
         # reinitialize inbound weights
         w, bias = self._reinit()
+
+        # TODO: what is the effect of inbound bias
         self.inbound.weight.data[idxs] = w[idxs]
         self.inbound.bias.data[idxs] = bias[idxs]
+
+        # TODO: what is the effect of outbound bias
         # set outbound weights to zero
         self.outbound.weight.data[:, idxs] = 0
+
         # reset the running average for reseted neurons
         self.module.running_avg[idxs] = 0
 
@@ -86,21 +92,30 @@ def apply_redo_parametrization(net, tau=0.005):
     supported_layers = (nn.ReLU, nn.LayerNorm, nn.Linear, nn.Conv2d)
     layers = [(k, v) for k, v in net.named_modules() if isinstance(v, supported_layers)]
     hndlrs = []
+    ratios = []
+    scores = []
     for i, (_, module) in enumerate(layers):
         if isinstance(module, nn.ReLU):
             inbound, outbound = layers[i - 1], layers[i + 1]
-            hook = ReDo.hook(module, inbound[1], outbound[1], tau=tau)
+            hook = ReDo.hook(module, inbound[1], outbound[1])
             hndlrs.append(hook)
+            ratios.append((module, hook))
+            scores.append((module, hook))
             print(f"Hooking {inbound[1]} -> {outbound[1]}")
 
-    # monkey-patch the estimator **instance** by bounding the method bellow
-    # to it. This way we can access the handlers.
+    # monkey-patch the estimator **instance** by bounding the method below it. 
+    # This way we can access the handlers.
     def get_dormant_ratios(self):
         ratios = [h.get_ratio().item() for h in hndlrs]
         mus = [h.get_avg_running_avg().item() for h in hndlrs]
         return ratios, mus
 
+    def get_dormant_scores(self):
+        scores = [h.get_score() for h in hndlrs]
+        return scores
+
     net.get_dormant_ratios = get_dormant_ratios.__get__(net)
+    net.get_dormant_scores = get_dormant_scores.__get__(net)
 
     return net
 
